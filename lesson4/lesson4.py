157f398dd8eddf4e9a8e3724721e088669f5a19b
Dhayalan <murugesandhayalan609@gmail.com>
2026-07-02 12:50:20 +0530

    day 1 code update

--- lesson4/lesson4.py
+++ lesson4/lesson4.py
@@ -1,6 +1,17 @@
-age=input("Enter your age:")
+# Try this WITHOUT casting first (comment out the int() lines to see the error)
+age = input("Enter your age: ")
 print(type(age))
 
-age=int(age)
+age = int(age)
 print(type(age))
-print("Next year you will be:"+(age+1))
+print("Next year you will be:", age + 1)
+
+# float example
+price = "19.99"
+price = float(price)
+print(price * 2)
+
+# str example
+number = 100
+text = str(number)
+print("The number is " + text)
